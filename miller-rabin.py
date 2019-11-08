fea3ea03830cd81bbb8bebd5abca2aa7e818714d
Allahcadrimbo <allahcadrimbo@gmail.com>
2019-11-07 23:22:04 -0600

    "Cleaned some of the code and comments up. Wrote out the algorithm for the miller-rabin test."

--- miller-rabin.py
+++ miller-rabin.py
@@ -1,65 +1,90 @@
+"""
+Author: Grant
+Date: 11/7/19
+
+Algorithm:
+    - Retrieve two user inputs
+        - n: An integer to be tested for primality
+        - w: The number of witnesses to check for the compositness of num
+    - Perform Miller-Rabin Test
+        - Find n - 1 = 2^k * q with q odd
+            - q := (n-1)/2^k
+        - Choose an integer a where 1 < a < n - 2 and repeat this w times over
+             - If n is prime and gcd(a, n) = 1 then one of the following rules holds:
+                - rule 1: a^q (mod n) = 1 or -1
+                - rule 2: (a^(2^i)*q) = -1 for some 0 <= i <= k - 1
+             - Compute rule_1  = a^q mod n
+                - If rule_1 is equal to 1 or n-1
+                    - This value is a non-witness for the compositeness of n
+                    - Add the value a to our witness list
+                    - continue to the next value of a
+                - Else compute rule_2 and do this k - 1 times over
+                    - rule_2 = rule_1^2 mod n
+                        - If rule_2 == 1
+                            - This value a is a witness for the compositeness of n
+                            - return False as we know n is composite
+                        - If rule_2 == n-1
+                            - This value a is a non-witness for the compositeness of n
+        - Return True, none of the w a values were witnesses
+        - If n was probably prime print the entire list (non witnesses)
+        - If n was composite print the last element in the list, which is a witness for compositness
+"""
 from random import randrange
 
-'''
-1. Find n - 1 = 2^k * m
-2. Choose a: 1 <
-'''
 
-
+# Tests for the primality of the inputted number n using the Miller-Rabin test.
+# @:param n The integer being tested
+# @:param witnesses The list hold the witnesses/non-witnesses
+# @:return True if it is probably prime and False if it is composite"""
 def miller_rabin(n, w, witnesses):
-    # If n is even then it is composite
+    # If n is even then it is composite. No need to continue.
     if n % 2 == 0:
         return False
 
     k = 0
     q = n - 1
-    while q % 2 == 0:  # If s mod 2 is not equal to 3 that means there is going to be a remainder. This means the next value of k in 2^k would result in not an int so we stop here
-        q //= 2  # We do floor division be cause we only want integers
+    # Finds the largest value of k that satiates n - 1 = 2^k * q
+    while q % 2 == 0:
+        q //= 2  # Use the floor division operator to ensure integer division
         k += 1
 
-    assert n - 1 == 2 ** k * q  # Step 2
-
-    print("k: " + str(k) + " q: " + str(q))
+    assert n - 1 == pow(2, k) * q
 
     for i in range(w):
         a = randrange(2, n - 1)
-        print("a: " + str(a))
         rule_1 = pow(a, q, n)
-        print("rule 1: " + str(rule_1))
+
+        # Rule 1 check
         if rule_1 == 1 or rule_1 == n - 1:
+            # might be prime
             witnesses.append(a)
             continue
 
+        # Rule 2 check
         for j in range(k):
             rule_2 = pow(rule_1, 2, n)
-            print("rule 2: " + str(rule_2))
 
-            if rule_2 == 1:
-                print(str(a))
+            if rule_2 == 1:  # n is composite
                 witnesses.append(a)
                 return False
-                # composite
-            elif rule_2 == n - 1:
+            elif rule_2 == n - 1:  # might be prime
                 witnesses.append(a)
-                # probs prime
 
     return True
-
-    # choose a where 1 < a < n - 2
-    # if 2**s mod n is congruent to 1 or -1 might be prime so go check next value
-    # if failed above check 2**s*2 if +1 its composite if -1 porbs prime
-    # list of witnesses and value of composite proof
 
 
 if __name__ == '__main__':
     while True:
         num = int(input("Enter a number to be tested for primality: "))
         num_witnesses = int(input("Enter the number of witnesses you want to test for: "))
+
         witnessesList = list()
-        # Find n -1 = 2^k * q
+
         if miller_rabin(num, num_witnesses, witnessesList):
+            # n is probably prime
             print("The number " + str(num) + " is probably prime. Here are its "
                   + str(num_witnesses) + " non-witnesses: " + str(witnessesList[:]))
         else:
-            print("The number " + str(num) + " is composite here is its witness of compossitness: "
+            # n is not prime
+            print("The number " + str(num) + " is composite here is its witness of compositeness: "
                   + str(witnessesList[-1:]))
